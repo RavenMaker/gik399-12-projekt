67589a726d0edbdf142c56b82be8a6d376f13d49
Croniz00 <philiop007@gmail.com>
2026-01-13 12:45:34 +0100

    Bort med jacks html igen?

--- javascript/backend/movies.sql
+++ javascript/backend/movies.sql
@@ -18,7 +18,7 @@ INSERT INTO movies (title, year, category, director) VALUES ('Forrest Gump', 199
 INSERT INTO movies (title, year, category, director) VALUES ('Parasite', 2019, 'Thriller', 'Bong Joon-ho');
 INSERT INTO movies (title, year, category, director) VALUES ('Interstellar', 2014, 'Science Fiction', 'Christopher Nolan');
 INSERT INTO movies (title, year, category, director) VALUES ('Gladiator', 2000, 'Historical Drama', 'Ridley Scott');
-insert into movies (title, year, category director ) VALUES ("sagaon om att klara kursen", 2026, "GIK399", "grupp 12");
+insert into movies (title, year, category director ) VALUES ("Sagan om att klara kursen", 2026, "GIK399", "grupp 12");
 
 -- Hämtar alla filmer--
 Select * from movies;
